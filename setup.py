b71fc40f73f455ae17e872a62eec359f0ace2a50
Deepak Sahoo <deepakksahoo1998@gmail.com>
2026-04-21 18:17:01 +0530

    Project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Deepak Sahoo",
+    author_email="deepakksahoo1998@gmail.com",
+    packages=find_packages()
+)
